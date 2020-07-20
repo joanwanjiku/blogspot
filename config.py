3db576da1c654325524b2472ef35c64ff87f576a
Joan_E <joanevans18@gmail.com>
2020-07-20 11:30:08 +0300

    deployment

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ class Config:
     SECRET_KEY =os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost/blog'
